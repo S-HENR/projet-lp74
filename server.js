e1a289e627e789eece118d5122a8f52751b77759
S-HENR <seraphin.henry@orange.fr>
2020-06-24 18:30:10 +0200

    push to heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/projet-lp74'));
+app.use(express.static('./dist/projetLP74'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/projet-lp74/'}),
+    res.sendFile('index.html', {root: 'dist/projetLP74/'}),
 );
 
 app.listen(process.env.PORT || 8080);
