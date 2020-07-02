b9510fe1856edcf7f83db5970e11dcafb421fcc3
S-HENR <seraphin.henry@orange.fr>
2020-07-02 17:02:35 +0200

    modal added

--- src/app/vocabulary/vocabulary-quiz/vocabulary-quiz.component.ts
+++ src/app/vocabulary/vocabulary-quiz/vocabulary-quiz.component.ts
@@ -9,13 +9,32 @@ import { ActivatedRoute } from '@angular/router';
 export class VocabularyQuizComponent implements OnInit {
 
   id:number;
+  modal_success;
+  modal_error;
+  close_button;
 
   constructor(
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     this.id = +this.route.snapshot.paramMap.get('id');
+    this.modal_success = document.getElementById('successModal');
+    this.modal_error = document.getElementById('errorModal');
+    this.close_button = document.getElementById('close');
+  }
+
+  toggleModal(id_modal: string) {
+    if(id_modal == "successModal")
+      if(this.modal_success.style.display == "none")
+        this.modal_success.style.display = "block";
+      else
+        this.modal_success.style.display = "none";
+    if(id_modal == "errorModal")
+      if(this.modal_error.style.display == "none")
+        this.modal_error.style.display = "block";
+      else
+        this.modal_error.style.display = "none";
   }
 
 }
